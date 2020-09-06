d5d2f835d0380af513c28d75a92233d43e6a5f9e
maxzettor <max.zettor@gmail.com>
2020-09-06 18:26:04 +0400

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 
+
+ const mockUserData=[
+ {name:'Max'},
+ {name:'Edvil'}
+]
+
+app.get('/users',function(req,res){
+		res.json({
+			sucess: true,
+			message: 'sucessfully got users. Super!',
+			users: mockUserData
+		})
+})
+
 app.listen(8000,function(){
 	console.log("server is running")
 })
